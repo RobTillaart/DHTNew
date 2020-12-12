129b292a75a8e5de4713b35613c0e7d773634683
rob tillaart <rob.tillaart@gmail.com>
2020-12-12 12:23:02 +0100

    initial arduino-ci + unit test + readme + getType()

---
+++ test/unit_test_001.cpp
@@ -0,0 +1,113 @@
+//
+//    FILE: unit_test_001.cpp
+//  AUTHOR: Rob Tillaart
+//    DATE: 2020-12-03
+// PURPOSE: unit tests for the DHT temperature and humidity sensor
+//          https://github.com/RobTillaart/DHTNew
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// ----------------------------
+// assertEqual(expected, actual)
+// assertNotEqual(expected, actual)
+// assertLess(expected, actual)
+// assertMore(expected, actual)
+// assertLessOrEqual(expected, actual)
+// assertMoreOrEqual(expected, actual)
+// assertTrue(actual)
+// assertFalse(actual)
+// assertNull(actual)
+
+#include <ArduinoUnitTests.h>
+
+#include "Arduino.h"
+#include "dhtnew.h"
+
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+unittest(test_constructor)
+{
+  DHTNEW dht(4);
+
+  // verify default flags
+  assertEqual(0, dht.getType());
+  assertEqual(0, dht.getHumOffset());
+  assertEqual(0, dht.getTempOffset());
+  assertEqual(0, dht.getDisableIRQ());
+  assertEqual(0, dht.getWaitForReading());
+  assertEqual(0, dht.getReadDelay());
+  assertEqual(0, dht.getSuppressError());
+}
+
+unittest(test_hum_temp)
+{
+  DHTNEW dht(4);
+
+  assertEqual(0, dht.getHumidity());
+  assertEqual(0, dht.getHumOffset());
+  dht.setHumOffset(1.5));
+  assertEqual(1.5, dht.getHumOffset());
+  
+  assertEqual(0, dht.getTemperature());
+  assertEqual(0, dht.getTempOffset());
+  dht.setTempOffset(-1.5));
+  assertEqual(-1.5, dht.getTempOffset());
+}
+
+unittest(test_process_flags)
+{
+  DHTNEW dht(4);
+
+  dht.setType(11));
+  assertEqual(11, dht.getType());
+  dht.setType(22));
+  assertEqual(22, dht.getType());
+  
+  dht.setDisableIRQ(true));
+  assertTrue(dht.getDisableIRQ());
+  dht.setDisableIRQ(false));
+  assertFalse(dht.getDisableIRQ());
+  
+  dht.setWaitForReading(true));
+  assertTrue(dht.getWaitForReading());
+  dht.setWaitForReading(false));
+  assertFalse(dht.getWaitForReading());
+  
+  dht.setReadDelay(1500));
+  assertEqual(1500, dht.getReadDelay());
+  dht.setType(11));
+  dht.setReadDelay());
+  assertEqual(1000, dht.getReadDelay());
+  dht.setType(22));
+  dht.setReadDelay());
+  assertEqual(2000, dht.getReadDelay());
+  
+  dht.setSuppressError(true));
+  assertTrue(dht.getSuppressError());
+  dht.setSuppressError(false));
+  assertFalse(dht.getSuppressError());
+}
+
+unittest(test_read)
+{
+  DHTNEW dht(4);
+
+  int rc = dht.read();
+  fprintf(stderr, "%d\n", rc);
+  
+  long lr = dht.lastRead();
+  fprintf(stderr, "%ld\n", lr);
+
+}
+
+unittest_main()
+
+// --------
